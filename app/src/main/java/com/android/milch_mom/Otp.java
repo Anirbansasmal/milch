ed9fde329e2ce7c7f094957c229e63bc2e250381
anirban <asasmal1996@gmail.com>
2019-12-23 18:46:58 +0530

    first commit

---
+++ app/src/main/java/com/android/milch_mom/Otp.java
@@ -0,0 +1,13 @@
+package com.android.milch_mom;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Otp extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otp);
+    }
+}
